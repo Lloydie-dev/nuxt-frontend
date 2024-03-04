91dc8df4e8e7889d2af3a1b88890ebec5245da0c
Lloydie <lloydie.glophics@gmail.com>
2024-03-04 11:01:56 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        {{ sample }}
+    </div>
+</template>
+
+<script >
+export default {
+    data() {
+        return {
+            sample: 'fsdfdsfdsf'
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
